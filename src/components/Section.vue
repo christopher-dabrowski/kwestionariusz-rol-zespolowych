af365a307b4489003959ced598c1401f225c4d7c
LuciusCalvius <lukaszz.mitoraj@gmail.com>
2020-08-13 22:23:25 +0200

    Adding option

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -4,92 +4,60 @@
     <h2>Część I</h2>
     <h4>Sądzę, że osobiście wnoszę do grupy...</h4>
     <p>
-      Do przydziału: <b>{{ left }}</b> punktów
+      Do przydziału:
+      <b>{{ left }}</b> punktów
     </p>
     <b-button class="mb-2" variant="primary" @click="reset">Reset</b-button>
 
     <!-- TODO: Generate options -->
-    <Option
+    <!-- <Option
       letter="A"
       text="Wydaje mi się, że szybko dostrzegam i umiem wykorzystać
             nowe możliwości"
       v-model="a"
       :max="10"
       :left="left"
-    />
-    <Option
-      letter="B"
-      text="Mogę dobrze pracować z różnymi ludźmi"
-      v-model="b"
-      :max="10"
-      :left="left"
-    />
-    <Option
-      letter="C"
-      text='"Produkowanie" pomysłów to moja naturalna zdolność'
-      v-model="c"
-      :max="10"
-      :left="left"
-    />
-    <Option
-      letter="D"
-      text='Moja siła twi w tym, że potrafię z ludzi "wyciągnąć" to, co mają w sobie najlepszego, aby przyczynili się do osiągnięcia celów i zadań grupowych'
-      v-model="d"
-      :max="10"
-      :left="left"
-    />
-    <Option
-      letter="E"
-      text="Moja główna umięjętność polega na doprowadzaniu spraw do końca i wiąże się z efektywnością"
-      v-model="e"
-      :max="10"
-      :left="left"
-    />
-    <Option
-      letter="F"
-      text="Jestem w stanie przez jakiś czas zaakceptować niepopularność mojej osoby, jeśli prowadzi to do wartościowych wyników"
-      v-model="f"
-      :max="10"
-      :left="left"
-    />
-    <Option
-      letter="G"
-      text="Zwykle wyczuwam, co jest realistyczne i prawdopodobne, jeśli chodzi o osiągnięcie sukcesu"
-      v-model="g"
-      :max="10"
-      :left="left"
-    />
-    <Option
-      letter="H"
-      text="Zwykle mogę zaproponować jakieś alternatywne wyjście bez uprzedzeń i niechęci"
-      v-model="h"
-      :max="10"
-      :left="left"
-    />
+    /> -->
+   
+    <Option v-for="(question, index) in questions"
+    :key="index"
+    letter="A"
+    text='question'
+    :max="10"
+    :left="10"
+    ></Option>
   </form>
 </template>
 
 <script>
+import Option from './Option';
 export default {
+  components: {
+    Option
+  },
   props: {
     title: String,
     subtitle: String,
     points: Number,
-    questions: Array,
+    questions: Array
   },
   data() {
     const values = new Array(this.questions.length).fill(0);
     return {
-      values: values,
+      values: values
     };
   },
   computed: {
     letters() {
       // TODO: Implement me
-
-      return [];
-    },
-  },
+      const answers = [];
+      for (let i = 0; i < this.questions.length; i++) {
+        const letterCode = 'A'.charCodeAt(0) + i;
+        answers.push(String.fromCharCode(letterCode))
+      }
+      return answers;
+    }
+  }
 };
 </script>
 
