826840d60008b2c67e7140d721680830afa00d89
LuciusCalvius <lukaszz.mitoraj@gmail.com>
2020-08-19 13:02:08 +0200

    Adding more questions

--- src/App.vue
+++ src/App.vue
@@ -46,7 +46,7 @@ export default {
       f: 0,
       g: 0,
       h: 0,
-      questions: ['Wydaje mi się, że szybko dostrzegam i umiem wykorzystać nowe możliwości', 'Mogę dobrze pracować z różnymi ludźmi', '"Produkowanie" pomysłów to moja naturalna zdolność'], 
+      questions: ['Wydaje mi się, że szybko dostrzegam i umiem wykorzystać nowe możliwości', 'Mogę dobrze pracować z różnymi ludźmi', '"Produkowanie" pomysłów to moja naturalna zdolność', 'Moja siła tkwi w tym, że potrafię z ludzi "wyciągnąć" to, co mają w sobie najlepszego, aby przyczynili się do osiągnięcia celów i zadań grupowych', 'Moja główna umiejętność polega na doprowadzaniu do końca i wiąże się z efektywnością', 'Jestem w stanie przez jakiś czas zaakceptować niepopularność mojej osoby, jeśli prowadzi do wartościowych wyników', 'Zwykle wyczuwam, co jest realistyczne i prawdopodobne, jeśli chodzi o osiągnięcie sukcesu', 'Zwykle mogę zaproponować jakieś alternatywne wyjście bez uprzedzeń i niechęci'], 
     };
   },
   computed: {
