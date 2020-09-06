01715c2c20a1c936c5816efc53e4967508cab195
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-09-06 14:15:16 +0200

    Remove comments

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,6 @@
       sub-title="Na użytek pracy licencjackiej"
       style="max-width: 1000px; margin: auto"
     >
-      <!-- <Section
-      title = "Część I"
-      subtitle = "Sądzę, że osobiście wnoszę do grupy..."
-      :points = "10"
-      :questions = "questions"
-      />
-      <hr/>
-      <Section
-      title = "Część II"
-      subtitle = "Lorem ipsum"
-      :points = "10"
-      :questions = "questions2"
-      />-->
       <div v-for="(section, index) in sections" :key="index">
         <Section
           :title="section.title"
@@ -50,61 +37,11 @@ export default {
   data() {
     return {
       sections: sections,
-      // total: 10,
-      // a: 0,
-      // b: 0,
-      // c: 0,
-      // d: 0,
-      // e: 0,
-      // f: 0,
-      // g: 0,
-      // h: 0,
-      // questions: [
-      //   "Wydaje mi się, że szybko dostrzegam i umiem wykorzystać nowe możliwości",
-      //   "Mogę dobrze pracować z różnymi ludźmi",
-      //   '"Produkowanie" pomysłów to moja naturalna zdolność',
-      //   'Moja siła tkwi w tym, że potrafię z ludzi "wyciągnąć" to, co mają w sobie najlepszego, aby przyczynili się do osiągnięcia celów i zadań grupowych',
-      //   "Moja główna umiejętność polega na doprowadzaniu do końca i wiąże się z efektywnością",
-      //   "Jestem w stanie przez jakiś czas zaakceptować niepopularność mojej osoby, jeśli prowadzi do wartościowych wyników",
-      //   "Zwykle wyczuwam, co jest realistyczne i prawdopodobne, jeśli chodzi o osiągnięcie sukcesu",
-      //   "Zwykle mogę zaproponować jakieś alternatywne wyjście bez uprzedzeń i niechęci"
-      // ],
-      // questions2: ["123"]
     };
   },
 
-  computed: {
-    // spend() {
-    //   let values = [
-    //     this.a,
-    //     this.b,
-    //     this.c,
-    //     this.d,
-    //     this.e,
-    //     this.f,
-    //     this.g,
-    //     this.h,
-    //   ];
-    //   const x = values.map(Number).reduce((a, b) => a + b, 0);
-    //   console.log("Wydano: " + x);
-    //   return x;
-    // },
-    // left() {
-    //   console.log("Zostało: " + (this.total - this.spend));
-    //   return this.total - this.spend;
-    // },
-  },
+  computed: {},
   methods: {
-    // reset() {
-    //   this.a = 0;
-    //   this.b = 0;
-    //   this.c = 0;
-    //   this.d = 0;
-    //   this.e = 0;
-    //   this.f = 0;
-    //   this.g = 0;
-    //   this.h = 0;
-    // },
     addRandomStuffToFB() {
       // TODO: Remove me!
 
