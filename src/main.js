a4115eb03f89272dcce708d168cd954ad7f6949e
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-09-06 22:57:51 +0200

    Add Font Awesome

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import { BootstrapVue } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPaperPlane);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
